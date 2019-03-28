c4c2c661388a80b6ee66da1318c4596a93c61629
= <=>
2019-03-28 17:12:24 +0800

    add columns name and a filter for admin.py

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import Article
 # Register your models here.
 
-admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title','content','pub_date')
+    list_filter = ['pub_date']
+
+
+admin.site.register(Article,ArticleAdmin)
--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,8 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Article(models.Model):
-    title = models.CharField(max_length=10)
-    content = models.TextField()
+    title = models.CharField(max_length=10,verbose_name='题目')
+    content = models.TextField(verbose_name='内容')
+    pub_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
